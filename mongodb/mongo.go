12b7a54571c9bd4ba9594e0d1c272c3cbd92fb37
king <k3360@qq.com>
2023-09-01 23:11:58 +0800

    bak

--- mongodb/mongo.go
+++ mongodb/mongo.go
@@ -9,13 +9,13 @@ import (
 )
 
 type MongoServer struct {
+	MongoURI string
+	Database string
 	*mongo.Client
-	mongoURI string
-	database string
 }
 
 func (s *MongoServer) Connect() (*MongoServer, error) {
-	clientOptions := options.Client().ApplyURI(s.mongoURI)
+	clientOptions := options.Client().ApplyURI(s.MongoURI)
 	// 连接MongoDB
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
@@ -30,7 +30,7 @@ func (s *MongoServer) Connect() (*MongoServer, error) {
 // 批量插入Document
 func (s *MongoServer) InsertMany(tableName string, documents []interface{}) error {
 	// 获取集合引用
-	collection := s.Client.Database(s.database).Collection(tableName)
+	collection := s.Client.Database(s.Database).Collection(tableName)
 	// 插入数据
 	_, err := collection.InsertMany(context.Background(), documents)
 	return err
@@ -38,7 +38,7 @@ func (s *MongoServer) InsertMany(tableName string, documents []interface{}) erro
 
 // 插入一条Document
 func (s *MongoServer) InsertOne(tableName string, document interface{}) error {
-	collection := s.Client.Database(s.database).Collection(tableName)
+	collection := s.Client.Database(s.Database).Collection(tableName)
 	// 插入数据
 	_, err := collection.InsertOne(context.Background(), document)
 	return err
@@ -46,7 +46,7 @@ func (s *MongoServer) InsertOne(tableName string, document interface{}) error {
 
 // 全量查询
 func (s *MongoServer) FindMany(tableName string, filter bson.M) ([]bson.Raw, error) {
-	collection := s.Client.Database(s.database).Collection(tableName)
+	collection := s.Client.Database(s.Database).Collection(tableName)
 	// 查询数据
 	cursor, err := collection.Find(context.Background(), filter)
 	if err != nil {
@@ -62,7 +62,7 @@ func (s *MongoServer) FindMany(tableName string, filter bson.M) ([]bson.Raw, err
 
 // 查询一条
 func (s *MongoServer) FindOne(tableName string, filter bson.M) (bson.Raw, error) {
-	collection := s.Client.Database(s.database).Collection(tableName)
+	collection := s.Client.Database(s.Database).Collection(tableName)
 	// 查询数据
 	res := collection.FindOne(context.Background(), filter)
 	bytes, err := res.DecodeBytes()
@@ -78,21 +78,21 @@ func (s *MongoServer) FindOne(tableName string, filter bson.M) (bson.Raw, error)
 
 // 只更新一条
 func (s *MongoServer) UpdateOne(tableName string, update bson.M, filter bson.M) error {
-	collection := s.Client.Database(s.database).Collection(tableName)
+	collection := s.Client.Database(s.Database).Collection(tableName)
 	_, err := collection.UpdateOne(context.Background(), filter, update)
 	return err
 }
 
 // 全量更新
 func (s *MongoServer) UpdateMany(tableName string, update bson.M, filter bson.M) error {
-	collection := s.Client.Database(s.database).Collection(tableName)
+	collection := s.Client.Database(s.Database).Collection(tableName)
 	_, err := collection.UpdateMany(context.Background(), filter, update)
 	return err
 }
 
 // 统计条数
 func (s *MongoServer) Count(tableName string, filter bson.M) (int64, error) {
-	collection := s.Client.Database(s.database).Collection(tableName)
+	collection := s.Client.Database(s.Database).Collection(tableName)
 
 	return collection.CountDocuments(context.Background(), filter)
 }
