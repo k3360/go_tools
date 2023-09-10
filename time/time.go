c9e02a0a8a3fcc814ae0931cb0d1c827282395bb
king <k3360@qq.com>
2023-09-10 12:51:59 +0800

    bak

--- time/time.go
+++ time/time.go
@@ -1,15 +1,29 @@
 package _time
 
 import (
+	"log"
 	"time"
 )
 
-func NowString() (string, error) {
+func Now() time.Time {
+	locTime := time.Now()
 	loc, err := time.LoadLocation("Asia/Shanghai")
 	if err != nil {
-		return "", err
+		log.Println("时间本地化失败", err)
+	} else {
+		locTime = locTime.In(loc)
 	}
-	locTime := time.Now().In(loc)
-	currentTime := locTime.Format("2006-01-02 15:04:05")
-	return currentTime, nil
+	return locTime
+}
+
+func NowString() string {
+	return Now().Format("2006-01-02 15:04:05")
+}
+
+func NowAdd(d time.Duration) time.Time {
+	return Now().Add(d)
+}
+
+func NowAddString(d time.Duration) string {
+	return NowAdd(d).Format("2006-01-02 15:04:05")
 }
