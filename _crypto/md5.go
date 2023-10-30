369aaf0583e81eb09e230132a984df8f00434ad2
king <k3360@qq.com>
2023-10-30 16:09:56 +0800

    bak

--- _crypto/md5.go
+++ _crypto/md5.go
@@ -1,11 +1,11 @@
-package _md5
+package _crypto
 
 import (
 	"crypto/md5"
 	"encoding/hex"
 )
 
-func Encode(value string) string {
+func MD5(value string) string {
 	// 计算 MD5 哈希值
 	hash := md5.Sum([]byte(value))
 	// 将哈希值转换为十六进制字符串
